6b20b667f2f91f736e511ff9d1a8be09b3821e30
morozvitali <morozvitali99@gmail.com>
2021-12-21 17:32:38 +0200

    little task working

--- src/Method/Test14.java
+++ src/Method/Test14.java
@@ -1,14 +1,16 @@
 package Method;
 import java.util.Objects;
 public class Test14 {
-    public static String[] filterOut(String filterString, String[][] array) {
-        String[] resultFiltering = new String[0];
+    public static String[][] filterOut(String filterString, String[][] array) {
+        String [][] resultFiltering = new String[array.length][];
+        int a = 0;
         for (int i = 0; i < array.length; i++) {
             for (int y = 0; y < array[i].length; y++) {
-
                 if (Objects.equals(array[i][y], filterString)) {
-                    resultFiltering = array[i];
-                    break;
+                    resultFiltering[i] = array[i];
+                } else {
+                   // resultFiltering[i] = null;
+
                 }
             }
         }
@@ -23,19 +25,22 @@ public class Test14 {
                 "возвращает массив строк которые содержат данную подстроку. " +
                 "т.е. отфильтрованый из первого");
 
-        String someStrData = "dolore";
+        String someStrData = "sit";
 
         String[][] stringArray = {{"Lorem", "ipsum", "dolor", "sit"},
-                {"amet", "consectetur", "adipiscing", "elit"},
-                {"sed", "do", "eiusmod", "tempor", "incididunt"},
+                {"amet", "consectetur", "adipiscing", "elit", "sit"},
+                {"sed", "do", "eiusmod", "tempor", "incididunt", "sit"},
                 {"ut", "labore", "et", "dolore", "magna", "aliqua"}};
 
 
-        String[] a = Test14.filterOut (someStrData, stringArray);
+        String[][] a = Test14.filterOut (someStrData, stringArray);
         // проверка корректности возврата значения..
         System.out.print("Выведем массив строк: ");
         for (int j=0; j<a.length; j++){
-            System.out.print(a[j] + " ");
+            for (int i=0;  i < a[j].length; i++) {
+                System.out.print(a[j][i] + " ");
+            }
+            System.out.println();
         }
     }
  }
