7e74975702316af3b9392404f5a7ed66847183ce
morozvitali <morozvitali99@gmail.com>
2022-01-10 17:09:04 +0200

    Test scanner11

--- src/Console/GuessNumber/GuessNumber.java
+++ src/Console/GuessNumber/GuessNumber.java
@@ -1,13 +1,36 @@
 package Console.GuessNumber;
+
 import java.util.Scanner; // импорт сканера из библиотеки классов
+import java.util.Random;
 
 public class GuessNumber {
     public static void main(String[] args) {
-        System.out.println("Введите число");
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        System.out.println("Введенное число: " + number);
-    }}
+        //опр мин и макс для поиска
+        //min - нижний предел
+        //max - верхний предел
+        int min = 0;
+        int max = 10;
+        //опр случайное число
+
+        Random rand = new Random();
+
+
+
+        int robotAnswer = min + rand.nextInt(max); // робот придумал число между мин - макс
+        System.out.println(robotAnswer + " - загадано роботом число"); // для проверки выведем его на экран, отладка
+//        int robotAnswer = 100;
+
+
+        System.out.println("Загадайте число");
+
+        Scanner scanner = new Scanner(System.in); // загадали число (ввести в консоль)
+        int userNumber = scanner.nextInt(); // присвоили заданное число userNumber
+        System.out.println("Введите ответ, загаданное Вами число: больше или меньше " + robotAnswer + "?"); // спросили больше меньше
+
+        String userAnswer = scanner.next(); // тут ответ больше или меньше
+        if (userAnswer == "больше") {min = robotAnswer;} else {max = robotAnswer;}
+    }
+}
 //// "Console game ""Guess number""
 ////Игра1:
 ////Пользователь загадывает число от 1 до 100, компьютер должен его угадать.
