73f3973f3780837561984c0c2395942044b3b950
morozvitali <morozvitali99@gmail.com>
2022-01-10 15:45:01 +0200

    Test scanner

--- src/Console/GuessNumber/GuessNumber.java
+++ src/Console/GuessNumber/GuessNumber.java
@@ -1,7 +1,13 @@
-//package Console.GuessNumber;
-//import java.util.Scanner;
-//
-//public class GuessNumber {
+package Console.GuessNumber;
+import java.util.Scanner; // импорт сканера из библиотеки классов
+
+public class GuessNumber {
+    public static void main(String[] args) {
+        System.out.println("Введите число");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("Введенное число: " + number);
+    }}
 //// "Console game ""Guess number""
 ////Игра1:
 ////Пользователь загадывает число от 1 до 100, компьютер должен его угадать.
@@ -19,7 +25,6 @@
 //            Метод SCANER (ввести больше или меньше чем compNumber)
 //            Переменная (предел уменьшается, становится больше или меньше в зависимости от ответа(ввода пользователем))
 //            if (compNumber > userNumber) {
-//    }
 //
 //    else {
 //        System.out.println("Write number, game ended");
